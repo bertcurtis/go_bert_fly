5dd88b322a5de907f7630140ebd8ba9b4a2643bd
bertcurtis <bert.curtis22@gmail.com>
2023-03-27 18:30:31 -0600

    reworking solve functions

--- internal/models/models.go
+++ internal/models/models.go
@@ -7,55 +7,155 @@ type FlightOutput struct {
 	Result string
 }
 
-func (fi FlightsInput) Solve() (fo FlightOutput, err error) {
-	departures, arrivals, err := fi.splitFlightsInput()
-	if err != nil {
-		return fo, nil
-	}
-	findInitialDeparture(departures, arrivals)
+//	func (fi FlightsInput) Solve() (fo FlightOutput, err error) {
+//		initial := findInitialDeparture(fi)
+//		return initial
+//	}
+func (fi FlightsInput) SolveInitial() []string {
+	initial := findDeparturePair(fi)
 
-	err = valdateInput(fi)
-	return fo, err
+	return initial
 }
 
-func valdateInput(fi FlightsInput) error {
-	return nil
+func (fi FlightsInput) SolveNext() []string {
+	initial := findDeparturePair(fi)
+	next := findNextDestinationPair(fi, initial)
+	return next
 }
-func findInitialDeparture(departures, arrivals []string) string {
-	initialDeparture := ""
-	for _, departuresI := range departures {
-		inArrivals := false
-		for _, arrivalsI := range arrivals {
-			if arrivalsI == departuresI {
-				inArrivals = true
+
+func findDeparturePair(fi FlightsInput) (returnPair []string) {
+	fmt.Println("Length of flights input: ", len(fi))
+	for _, flightPair := range fi {
+		isDeparture := true
+		fmt.Println("Input pair under iteration: ", flightPair)
+		fmt.Println("Is this departure in: ", flightPair[0])
+		for i := 0; i < len(fi); i++ {
+			fmt.Println("arrval: ", fi[i][1])
+			if flightPair[0] == fi[i][1] {
+				isDeparture = false
 				break
 			}
-			if !inArrivals {
-				initialDeparture = departuresI
-			}
-
+		}
+		if isDeparture {
+			fmt.Println("found:", flightPair)
+			returnPair = flightPair
+			break
 		}
 	}
-	return initialDeparture
+	return returnPair
 }
-func (fi FlightsInput) splitFlightsInput() (departures, arrivals []string, err error) {
-	for _, flightPair := range fi {
-		if len(flightPair) != 2 {
-			return departures, arrivals, fmt.Errorf("Item %v does not have exactly 2 airports.", flightPair)
-		}
-		// this iterates through each item in the list and puts the first value in the first list,
-		// and the second value in the second list turning a list of potentiallhy dozens of pairs into 2 lists
-		// of departures and arrivals
-		for i, airport := range flightPair {
-			if i == 0 {
-				departures = append(departures, airport)
-			} else {
-				arrivals = append(arrivals, airport)
-			}
 
+func findNextDestinationPair(fi FlightsInput, flightPair []string) (returnPair []string) {
+	for _, flightInputPair := range fi {
+		if flightInputPair[0] == flightPair[1] {
+			returnPair = flightInputPair
+			break
 		}
-
 	}
-	return departures, arrivals, nil
-
+	return returnPair
 }
+
+// func (fi FlightsInput) ValidateLengths() (arrivalsLength int, departuresLength int, err error) {
+// 	departures, arrivals, err := fi.splitFlightsInput()
+// 	if err != nil {
+// 		return 0, 0, err
+// 	}
+// 	return len(arrivals), len(departures), err
+// }
+
+//	func valdateInput(fi FlightsInput) error {
+//		return nil
+//	}
+// func findNextPair(fi FlightsInput, linkedDeparture string) {
+// 	orderedFlights := [][]string{}
+// 	for _, flightInputPair := range fi {
+// 		fmt.Println("Input pair under iteration: ", flightInputPair)
+// 		if flightInputPair[0] == linkedDeparture {
+// 			orderedFlights = append(orderedFlights, flightInputPair)
+// 			pairedArrival = flightInputPair[1]
+// 		}
+// 		orderedLen := len(orderedFlights)
+
+// 		if orderedLen > 0 {
+// 			linkedDeparture = orderedFlights[orderedLen-1][1]
+// 		}
+// 	}
+
+// }
+// func findInitialDeparture(fi FlightsInput) string {
+// 	fmt.Println("Flights Input: ", fi)
+// 	departures, arrivals, err := fi.splitFlightsInput()
+// 	if err != nil {
+// 		return "error"
+// 	}
+// 	initialDeparture := ""
+// 	fmt.Println("departures: ", departures)
+// 	fmt.Println("arrivals: ", arrivals)
+// 	for _, departuresI := range departures {
+// 		inArrivals := false
+// 		fmt.Println("Comparing this departure: ", departuresI)
+// 		for _, arrivalsI := range arrivals {
+// 			if departuresI == arrivalsI {
+// 				inArrivals = true
+// 				fmt.Println("This departure is an arrival. This is not the initial departure", departuresI)
+// 				break
+// 			}
+// 		}
+// 		if !inArrivals {
+// 			fmt.Println("There are no matching arrivals for this departure. Assuming this is the initial departure.", departuresI)
+// 			initialDeparture = departuresI
+// 			break
+// 		}
+// 	}
+// 	return initialDeparture
+// }
+
+// func findArrivalByDeparture(fi FlightsInput, departure string) (arrival string) {
+// 	for _, flightPair := range fi {
+// 		if flightPair[0] == departure {
+// 			arrival = flightPair[1]
+// 			break
+// 		}
+// 	}
+// 	return arrival
+// }
+
+// func addToOrderedList(fi FlightsInput, departure string) (arrival string){
+// 	orderedFlights := [][]string{}
+// 	linkedDeparture := initialDeparture
+// 	for _, flightInputPair := range fi {
+// 		fmt.Println("Input pair under iteration: ", flightInputPair)
+// 		pairedArrival := ""
+// 		if flightInputPair[0] == linkedDeparture {
+// 			orderedFlights = append(orderedFlights, flightInputPair)
+// 			pairedArrival = flightInputPair[1]
+// 		}
+// 		orderedLen := len(orderedFlights)
+
+// 		if orderedLen > 0 {
+// 			linkedDeparture = orderedFlights[orderedLen - 1][1]
+// 		}
+// 	}
+// }
+
+// func (fi FlightsInput) splitFlightsInput() (departures, arrivals []string, err error) {
+// 	for _, flightPair := range fi {
+// 		if len(flightPair) != 2 {
+// 			return departures, arrivals, fmt.Errorf("Item %v does not have exactly 2 airports.", flightPair)
+// 		}
+// 		// this iterates through each item in the list and puts the first value in the first list,
+// 		// and the second value in the second list turning a list of potentiallhy dozens of pairs into 2 lists
+// 		// of departures and arrivals
+// 		for i, airport := range flightPair {
+// 			if i == 0 {
+// 				departures = append(departures, airport)
+// 			} else {
+// 				arrivals = append(arrivals, airport)
+// 			}
+
+// 		}
+
+// 	}
+// 	return departures, arrivals, nil
+
+// }
--- internal/models/models_test.go
+++ internal/models/models_test.go
@@ -8,21 +8,69 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestModelsSolve(t *testing.T) {
+func TestFindInitialDeparture(t *testing.T) {
 	// this tells go that this test can run in Parallel
 	// with other t.parallel enabled unit tests
 	t.Parallel()
+	assert := assert.New(t)
 
-	sample := `[["SFO","SLC"],["ATL","JFK"],["PHX","ATL"],["SLC","PHX"]]`
+	sample := `[["ATL","JFK"],["PHX","ATL"],["SLC","PHX"], ["SFO","SLC"]]`
 	fi := models.FlightsInput{}
 
 	err := json.Unmarshal([]byte(sample), &fi)
 	// ensure no Unmarshal error
-	assert.Nil(t, err)
+	assert.Nil(err)
 
-	flightOutput, err := fi.Solve()
-	// should be no errors
-	assert.Nil(t, err)
+	// arrL, depL, err := fi.ValidateLengths()
+	// assert.Nil(err)
+	// assert.Equal(4, arrL)
+	// assert.Equal(4, depL)
 
-	assert.Equal(t, flightOutput.Result, "")
+	initial := fi.SolveInitial()
+	assert.Equal([]string{"SFO", "SLC"}, initial)
+
+	//assert.Equal(t, flightOutput.Result, "")
 }
+
+func TestFindNextDestination(t *testing.T) {
+	// this tells go that this test can run in Parallel
+	// with other t.parallel enabled unit tests
+	t.Parallel()
+	assert := assert.New(t)
+
+	sample := `[["ATL","JFK"],["PHX","ATL"],["SLC","PHX"], ["SFO","SLC"]]`
+	fi := models.FlightsInput{}
+
+	err := json.Unmarshal([]byte(sample), &fi)
+	// ensure no Unmarshal error
+	assert.Nil(err)
+
+	// arrL, depL, err := fi.ValidateLengths()
+	// assert.Nil(err)
+	// assert.Equal(4, arrL)
+	// assert.Equal(4, depL)
+
+	initial := fi.SolveNext()
+	assert.Equal([]string{"SLC", "PHX"}, initial)
+
+	//assert.Equal(t, flightOutput.Result, "")
+}
+
+// func TestModelsSolve(t *testing.T) {
+// 	// this tells go that this test can run in Parallel
+// 	// with other t.parallel enabled unit tests
+// 	t.Parallel()
+
+// 	sample := `[["SFO","SLC"],["ATL","JFK"],["PHX","ATL"],["SLC","PHX"]]`
+// 	fi := models.FlightsInput{}
+
+// 	err := json.Unmarshal([]byte(sample), &fi)
+// 	// ensure no Unmarshal error
+// 	assert.Nil(t, err)
+
+// 	flightOutput, err := fi.Solve()
+// 	// should be no errors
+// 	assert.Nil(t, err)
+
+// 	assert.Equal(t, flightOutput.Result, "")
+// }
